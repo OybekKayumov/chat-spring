b895f66963803ce7222cb00948d7125025137ab6
OybekKayumov <oybek.kayumov@gmail.com>
2024-08-26 14:18:30 +0500

    Add rest methods - base

--- src/main/java/org/example/ChatController.java
+++ src/main/java/org/example/ChatController.java
@@ -1,13 +1,35 @@
 package org.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class ChatController {
 
     @GetMapping("/init")
-    public String init() {
-        return "Yes";
+    public Boolean init() {
+
+        //todo: check sessionId, if found -> true
+//        return "Yes";
+        return true;
+
+    }
+
+    @PostMapping("/message")
+    public Boolean sendMessage(@RequestParam String message) {
+
+        return true;
+    }
+
+    @GetMapping("/message")
+    public List<String> getMessagesList() {
+        return null;
+    }
+
+    @GetMapping("/user")
+    public HashMap<Integer, String> getUsersList() {
+        return null;
     }
 }
